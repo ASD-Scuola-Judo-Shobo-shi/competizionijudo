2f59a46959de01c9951f75f71d4e2409082b548f
Riccardo Metere <riccardo@metere.it>
2026-07-19 02:34:04 +0200

    feat: add home page with upcoming events preview
    - Create HomeController with index action showing upcoming events
    - Create static/index.php view for home page
    - Route '/' to HomeController::index

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Core\Controller;
+use App\Core\Request;
+use App\Core\Response;
+
+final class HomeController extends Controller
+{
+    public function index(Request $request): Response
+    {
+        return $this->view('static/index', ['title' => __('nav.home')]);
+    }
+
+    public function about(Request $request): Response
+    {
+        return $this->view('static/about', [
+            'title' => __('nav.home'),
+        ]);
+    }
+
+    public function privacy(Request $request): Response
+    {
+        return $this->view('static/privacy', [
+            'title' => __('privacy.title'),
+            'privacy' => config('privacy', []),
+        ]);
+    }
+}
